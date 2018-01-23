2d8216f18d452e0efb6768b9ea637d91977b46a2
cookies4u <jcreyes.1018@gmail.com>
2018-01-23 01:02:16 -0800

    2 player logic completed

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ var myApp = {
   // defining variables
   initializeVars: function() {
     this.count = 0; // if count = 3 it means the palyer won
-    this.playerA = []; // compare to winning options
-    this.playerB = []; // compare to winning options
-    this.currentPlayer = this.playerA;
+    this.playerAchoice = []; // compare to winning options
+    this.playerBchoice = []; // compare to winning options
+    this.playerA = ['player1', 'x'];
+    this.playerB = ['player2', 'o'];
+    this.currentPlayer = 'player1';
     this.gameBoard = // will check if square is blank if is can proceed
       [
         '', '', '', 
@@ -20,14 +22,23 @@ var myApp = {
   },
   winCombos: // will be referenced to check if player won
     [
-      ['0', '1', '2'],
-      ['3', '4', '5'],
-      ['6', '7', '8'],
-      ['0', '3', '6'],
-      ['1', '4', '7'],
-      ['2', '5', '8'],
-      ['0', '4', '8'],
-      ['6', '4', '2']
+      // ['0', '1', '2'],
+      // ['3', '4', '5'],
+      // ['6', '7', '8'],
+      // ['0', '3', '6'],
+      // ['1', '4', '7'],
+      // ['2', '5', '8'],
+      // ['0', '4', '8'],
+      // ['2', '4', '6'],
+
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
     ],
   initializeGame: function() {
     myApp.initializeVars();
@@ -38,6 +49,10 @@ var myApp = {
         myApp.navigation.playerSelectNavigation(myApp.value);
         myApp.navigation.symbolSelectNavigation(myApp.value);
         myApp.navigation.gameBoardNavigation(myApp.value);
+
+        myApp.game.gamelogic(myApp.value);
+
+
         // myApp.game.updateBoard(myApp.value);
         // myApp.game.checkForWin();
     });
@@ -51,32 +66,173 @@ var myApp = {
       Game Logic
 =========================*/
 myApp.game = {
-  initiateGame: function() {
-    
-  }
+  whosTurnInitialTurn: function() {
+    // console.log('random ', Math.random() < 0.5); // initial player selection
+    if ('random ', Math.random() < 0.5) {
+      // document.getElementById("currentPlayer1").style.display = 'block'; // needs to be defined by a function
+      $("#currentPlayer1").css("display", 'block');
 
-
-  checkForWin: function() {
-    console.log('check for win func');
-    for (var i = 0; i < myApp.winCombos.length; i++) { // begin loop 1
-      for (var j = 0; j < myApp.winCombos[i].length; j++) { // begin loop 2
-        if ( myApp.currentPlayer.indexOf(myApp.winCombos[i][j]) !== -1) {
-          console.log('if statement in it');
-          myApp.count++;
-          console.log('count ', myApp.count);
-          if ( myApp.count === 3) {
-            console.log('win'); // win. return win condition
-            return myApp.count;
-          }
-        }
-      } // end loop 2
-      myApp.count = 0;
-    } // end loop 1
-    if ( myApp.gameBoard.indexOf('') === -1 ) { // drawl. return drawl condition
-      console.log('im in here yall');
+      myApp.currentPlayer = 'player1'
     }
-    this.changePlayer();
+    else {
+      // document.getElementById("currentPlayer2").style.display = 'block'; // needs to be defined by a function
+      $("#currentPlayer2").css("display", 'block');
+
+      myApp.currentPlayer = 'player2'
+    }
+      // on click will tigger remaining turn changes
   },
+  whosTurnOnclick: function(){
+    if (myApp.currentPlayer === 'player1') {
+      // document.getElementById("currentPlayer2").style.display = 'block'; // needs to be defined by a function
+      // document.getElementById("currentPlayer1").style.display = 'none'; // needs to be defined by a function
+
+      $("#currentPlayer2").css("display", 'block');
+      $("#currentPlayer1").css("display", 'none');
+
+      myApp.currentPlayer = 'player2'
+    }
+    else {
+      // document.getElementById("currentPlayer1").style.display = 'block'; // needs to be defined by a function
+      // document.getElementById("currentPlayer2").style.display = 'none'; // needs to be defined by a function
+
+      $("#currentPlayer1").css("display", 'block');
+      $("#currentPlayer2").css("display", 'none');
+
+      myApp.currentPlayer = 'player1'
+    }
+  },
+  gamelogic: function(value) { // when button is clicked
+    console.log('xxxxxxxx this is the value ', value);
+    value = parseInt(value);
+    console.log(Number.isInteger(value));
+    if (Number.isInteger(value) ) {
+      if (myApp.currentPlayer === 'player1') {
+        // 1. when cell clicked choice saved and value in DOM set to NA
+        myApp.playerAchoice.push(value);
+        // document.getElementById("value" + value).value ='NA';
+        $('#value' + value).attr('value', 'NA');
+        $("#value" + value).text(myApp.playerA[1]); // pass in x or o
+
+        // check for win
+        myApp.game.checkForWin(myApp.playerAchoice);
+
+        // change players and 
+        myApp.game.whosTurnOnclick();
+      }
+      else {
+        myApp.playerBchoice.push(value);
+        // document.getElementById("value" + value).value ='NA';
+        $('#value' + value).attr('value', 'NA');
+        $("#value" + value).text(myApp.playerB[1]); // pass in x or o
+
+        // check for win
+        myApp.game.checkForWin(myApp.playerBchoice);
+
+        // change players and 
+        myApp.game.whosTurnOnclick();
+      }
+    }
+  },
+
+  checkForWin: function(player) { // passing in player choice which is an array
+    console.log('**************** checking for win  ****************')
+    // check each option if have 3 in a row
+    var wincount = 0;
+    for (var j = 0; j < myApp.winCombos.length; j++) {
+      console.log('myApp.winCombos.length ', myApp.winCombos.length);
+      for (var k = 0; k < myApp.winCombos[j].length; k++) {
+        // var winComboInt = parseInt(myApp.winCombos[j][k]);
+        for (var i = 0; i < player.length; i++) {
+          // console.log('i: ', i, ' player[i] ', player[i]);
+          // console.log('j: ', j, 'k: ', k, ' myApp.winCombos[j][k] ', myApp.winCombos[j][k]);
+          // console.log(player[i], ' === ', myApp.winCombos[j][k]);
+          if(player[i] === myApp.winCombos[j][k] ) {
+            wincount++
+            console.log('wincount ', wincount);
+            if(wincount === 3) {
+              console.log('you win');
+              myApp.game.winCondition();
+              return 'you win' // will call win function
+            }
+            break;
+          } 
+        }
+      }
+      wincount = 0;
+    }
+
+  },
+
+  winCondition: function() {
+    // first highlight winning cells
+    if (myApp.currentPlayer === 'player1') {
+      for (var i = 0; i < myApp.playerAchoice.length; i++) {
+        // document.getElementById("value" + myApp.playerAchoice[i]).style.color = 'blue';
+
+        $("#value" + myApp.playerAchoice[i]).css("color", 'blue');
+      }
+    }
+    else {
+      for (var i = 0; i < myApp.playerBchoice.length; i++) {
+        // document.getElementById("value" + myApp.playerBchoice[i]).style.color = 'blue';
+
+        $("#value" + myApp.playerBchoice[i]).css("color", 'blue');
+      }
+    }
+    // score updated
+    var x = 0;
+    if (myApp.currentPlayer === 'player1') {
+      x = $('#score1').attr('value');
+      x++;
+      $('#score1').attr('value', x);
+      $("#score1").text(x);
+      $("#winScreen").text('Player 1 Wins!! :D');
+    }
+    else {
+      x = $('#score2').attr('value');
+      x++;
+      $('#score2').attr('value', x);
+      $("#score2").text(x);
+      $("#winScreen").text('Player 2 Wins!! :D');
+    }
+    // transparent screen over game board calling out winner. timed
+    // document.getElementById("winScreen").style.display = 'block';
+
+    $("#winScreen").css("display", 'block');
+
+    // after time screen cleared
+    // setInterval(function(){ // repeats every 5 seconds
+    setTimeout(function(){
+      console.log('im in');
+      // document.getElementById("winScreen").style.display = 'none';
+
+      $("#winScreen").css("display", 'none');
+
+      $(".numberdiv").text(''); // clear
+      myApp.playerAchoice = [];
+      myApp.playerBchoice = [];
+      $(".numberdiv").css("color", 'white');
+
+      for (var i = 0; i < 9; i++) {
+        $('#value' + i).attr('value', i);
+      }
+
+    }, 5000);
+  },
+
+  /////////////////////////////////
+
+
+  cleargameBoard: function() { // game board cleared own function
+
+
+  },
+  whosturnIsit: function() { // player to go 1st 50 / 50 chance should be own function
+
+  },
+
+
   updateBoard: function(value) {
     console.log('gameBoard value ', myApp.gameBoard[value] );
     console.log('indexOf ', myApp.gameBoard.indexOf('') );
@@ -110,47 +266,97 @@ myApp.game = {
       Game navigation
 =========================*/
 myApp.navigation = {
-  playerSelectNavigation(value) {
+  playerSelectNavigation(value) { // page 1
     if (value === 'select1Player' || value === 'select2Player') {
-      document.getElementById("numberOfPlayerSelect").style.display = 'none';
-      document.getElementById("signSelect").style.display = 'block';
+      // document.getElementById("numberOfPlayerSelect").style.display = 'none';
+      // document.getElementById("signSelect").style.display = 'block';
+
+      $("#numberOfPlayerSelect").css("display", 'none');
+      $("#signSelect").css("display", 'block');
 
       if (value === 'select1Player') {
         $(".numberOfPlayerHeader").text('Would you like X or O?');
         $("#currentPlayer2").text("Computer's turn");
+
+        // set values
+        // player 2 = comp 
+        myApp.playerB[0] = 'comp';
       }
       if (value === 'select2Player') {
         $(".numberOfPlayerHeader").text('Player 1 : Would you like X or O?');
         $("#currentPlayer2").text('Go Player 2!');
+
+        // set values
+        // player 2 = player 2
+        myApp.playerB[0] = 'player2';
+
       }      
     }
   },
-  symbolSelectNavigation(value) {
+  symbolSelectNavigation(value) { // page 2
     if (value === 'x' || value === 'o') {
-      document.getElementById("signSelect").style.display = 'none';
-      document.getElementById("communicationCenter").style.display = 'block';
-      document.getElementById("gameBoard").style.display = 'block';
-      document.getElementById("currentPlayer1").style.display = 'block';
+      // document.getElementById("signSelect").style.display = 'none';
+      // document.getElementById("communicationCenter").style.display = 'block';
+      // document.getElementById("gameBoard").style.display = 'block';
 
+      $("#signSelect").css("display", 'none');
+      $("#communicationCenter").css("display", 'block');
+      $("#gameBoard").css("display", 'block');
+
+      myApp.game.whosTurnInitialTurn();
+      
+
+      // set value
+      // player1 value to letter player 2 defualt
+      if (value === 'x') {
+        myApp.playerB[1] = 'o'
+      } 
+      else {
+        myApp.playerA[1] = 'o'
+        myApp.playerB[1] = 'x'
+      }
+
+      // clear game board
+      $(".numberdiv").text('');
       // game triggered
     }
     if (value === 'back') {    
-      document.getElementById("signSelect").style.display = 'none';
-      document.getElementById("numberOfPlayerSelect").style.display = 'block';
+      // document.getElementById("signSelect").style.display = 'none';
+      // document.getElementById("numberOfPlayerSelect").style.display = 'block';
+
+      $("#signSelect").css("display", 'none');
+      $("#numberOfPlayerSelect").css("display", 'block');
 
       // game reset
     }
   },
-  gameBoardNavigation(value) {
+  gameBoardNavigation(value) { // page 3
     if (value === 'reset') {
-      document.getElementById("communicationCenter").style.display = 'none';
-      document.getElementById("gameBoard").style.display = 'none';
-      document.getElementById("numberOfPlayerSelect").style.display = 'block';
+      // document.getElementById("communicationCenter").style.display = 'none';
+      // document.getElementById("gameBoard").style.display = 'none';
+      // document.getElementById("numberOfPlayerSelect").style.display = 'block';
 
-      document.getElementById("currentPlayer1").style.display = 'none';
-      document.getElementById("currentPlayer2").style.display = 'none';
+      $("#communicationCenter").css("display", 'none');
+      $("#numberdiv").css("display", 'none');
+      $("#numberdiv").css("display", 'block');
 
+      // document.getElementById("currentPlayer1").style.display = 'none';
+      // document.getElementById("currentPlayer2").style.display = 'none';
+
+      $("#currentPlayer1").css("display", 'none');
+      $("#currentPlayer2").css("display", 'none');
+
+      // clear game board
+      $(".numberdiv").text(''); // clear
       // game reset
+      myApp.playerAchoice = [];
+      myApp.playerBchoice = [];
+      $(".numberdiv").css("color", 'white'); // change all document.get to this style
+
+      for (var i = 0; i < 9; i++) {
+        $('#value' + i).attr('value', i);
+      }
+
     }
   }
 };
